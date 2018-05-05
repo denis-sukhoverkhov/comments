4d9b69e8b9f33340cda8c4d84d21ff52b37689d4
Denis Sukhoverkhov <at10tion.angry@yandex.ru>
2018-05-05 22:40:32 +0300

    endpoint for getting comments of top layer from entity "comment"

--- comments/tests/functional/test_api_create_comment.py
+++ comments/tests/functional/test_api_create_comment.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from comments.models import Post
+from comments.models import Post, Comment
 
 
 class CreateCommentApiTest(TestCase):
@@ -19,7 +19,7 @@ class CreateCommentApiTest(TestCase):
         self.client = APIClient()
         self.client.force_authenticate(user=self.authorized_user)
 
-    def test_api_create_comment(self):
+    def test_create_comment(self):
         content_type = ContentType.objects.get_for_model(Post)
         post = Post.objects.create(title='custom title', body='body')
         data = {'body': 'interesting',
@@ -36,3 +36,73 @@ class CreateCommentApiTest(TestCase):
         self.assertEqual(result['content_type'], data['content_type'])
         self.assertEqual(result['object_id'], data['object_id'])
         self.assertTrue('created' in result)
+
+    def test_get_comments_for_entity_comment_if_child_comments_not_exists(self):
+        post = Post.objects.create(title='custom title', body='body')
+        c_type = ContentType.objects.get_for_model(post)
+        parent_comment = Comment.objects.create(body='parent',
+                                                user=self.authorized_user,
+                                                content_type=c_type,
+                                                object_id=post.id)
+        response = self.client.get(f'/api/comment/{parent_comment.id}/comment/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, [])
+
+    def test_get_comments_for_entity_comment_if_child_comments_exists(self):
+        post = Post.objects.create(title='custom title', body='body')
+        parent_comment = Comment.objects.create(body='parent',
+                                                user=self.authorized_user,
+                                                content_type=ContentType.objects.get_for_model(post),
+                                                object_id=post.id)
+        c_type = ContentType.objects.get_for_model(Comment)
+        child1 = Comment.objects.create(body='parent',
+                                        user=self.authorized_user,
+                                        content_type=c_type,
+                                        object_id=parent_comment.id)
+        child2 = Comment.objects.create(body='parent',
+                                        user=self.authorized_user,
+                                        content_type=c_type,
+                                        object_id=parent_comment.id)
+        child3 = Comment.objects.create(body='parent',
+                                        user=self.authorized_user,
+                                        content_type=c_type,
+                                        object_id=parent_comment.id)
+        response = self.client.get(f'/api/comment/{parent_comment.id}/comment/')
+        child_list = [child1, child2, child3]
+        self.assertEqual(len(response.data), len(child_list))
+
+    # def test_get_comments_for_entity_post(self):
+    #     # content_type = ContentType.objects.get_for_model(Post)
+    #     # post = Post.objects.create(title='custom title', body='body')
+    #     # data = {'body': 'interesting',
+    #     #         'user': self.authorized_user.id,
+    #     #         'content_type': content_type.id,
+    #     #         'object_id': post.id,
+    #     #         }
+    #     response = self.client.post(f'/api/post/{post_id}/comment/', data, format='json')
+    #     # self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    #     # result = response.data
+    #     # # pass
+    #     # self.assertEqual(result['body'], data['body'])
+    #     # self.assertEqual(result['user'], data['user'])
+    #     # self.assertEqual(result['content_type'], data['content_type'])
+    #     # self.assertEqual(result['object_id'], data['object_id'])
+    #     # self.assertTrue('created' in result)
+    #
+    # def test_get_comments_for_entity_user(self):
+    #     # content_type = ContentType.objects.get_for_model(Post)
+    #     # post = Post.objects.create(title='custom title', body='body')
+    #     # data = {'body': 'interesting',
+    #     #         'user': self.authorized_user.id,
+    #     #         'content_type': content_type.id,
+    #     #         'object_id': post.id,
+    #     #         }
+    #     response = self.client.post(f'/api/user/{user_id}/comment/', data, format='json')
+    #     # self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    #     # result = response.data
+    #     # # pass
+    #     # self.assertEqual(result['body'], data['body'])
+    #     # self.assertEqual(result['user'], data['user'])
+    #     # self.assertEqual(result['content_type'], data['content_type'])
+    #     # self.assertEqual(result['object_id'], data['object_id'])
+    #     # self.assertTrue('created' in result)
--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets
+from django.contrib.contenttypes.models import ContentType
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from comments.models import Comment
 from comments.serrializers import CommentSerializer
@@ -8,3 +11,10 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
+
+    @action(detail=True, url_path='comment')
+    def child_comments(self, request, pk=None):
+        comment_list_qs = Comment.objects.filter(object_id=pk, content_type=ContentType.objects.get_for_model(Comment))
+
+        ser = self.serializer_class(comment_list_qs, many=True)
+        return Response(ser.data, status=status.HTTP_200_OK)
