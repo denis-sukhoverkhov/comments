b92564143fade485945490d1a73eeac64760fc9b
Denis Sukhoverkhov <at10tion.angry@yandex.ru>
2018-05-05 14:26:09 +0300

    added GenericforeignKey to model Comments for binding several entities

--- comments/models.py
+++ comments/models.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils.timezone import now
 from django.utils.translation import ugettext as _
@@ -8,6 +10,9 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='User')
     created = models.DateTimeField(default=now, verbose_name=_('Date created'))
     parent = models.ForeignKey('self', blank=True, null=True, on_delete=models.PROTECT, verbose_name='Parent')
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
 
 
 class Post(models.Model):
